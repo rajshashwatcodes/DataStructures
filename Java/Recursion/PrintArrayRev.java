84745b0849262cad926ddc63b0e944c8f830fa4d
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-03-05 23:43:08 +0530

    Create PrintArrayRev.java

---
+++ Java/Recursion/PrintArrayRev.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class PrintArrayRev {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the array length: ");
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for (int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        printArr(arr,n-1);
+    }
+
+    public static int printArr(int[] arr,int n) {
+        if (n == -1) 
+            return 0;
+        System.out.println(arr[n]);
+        printArr(arr,n-1); 
+        return n;
+    }
+}
+
+
