99111a5e5958f803d542c920ad5cfe0c084fe256
Shashwat Raj <113779833+rajshashwatcodes@users.noreply.github.com>
2023-04-05 23:50:30 +0530

    Create MatrixMultiplication.java

---
+++ Java/Matrix/MatrixMultiplication.java
@@ -0,0 +1,23 @@
+public class MatrixMultiplication {
+    public static void main (String args[]){
+        int[][] a = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] b = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+        int[][] c = new int[3][3];
+
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[i].length; j++) {
+                for (int k = 0; k < c.length; k++) {
+                    c[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+
+        System.out.println("The product of the matrices is");
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[i].length; j++) {
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
